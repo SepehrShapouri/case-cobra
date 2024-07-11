7113ba23adf9e148fffe38331121e20a8c5b2772
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-07-11 17:39:37 +0330

    feat:added model configuration

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -3,13 +3,20 @@ import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { Hand } from "lucide-react";
+import { Check, ChevronsUpDown, Hand } from "lucide-react";
 import NextImage from "next/image";
 import React, { useState } from "react";
 import { Rnd } from "react-rnd";
 import { RadioGroup } from "@headlessui/react";
-import { COLORS } from "@/validators/option-validator";
+import { COLORS, MODELS } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 interface DesignConfiguratorProps {
   configId: string;
   imgUrl: string;
@@ -20,8 +27,12 @@ function DesignConfigurator({
   imgUrl,
   imageDimensions,
 }: DesignConfiguratorProps) {
-  const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number];
+    model: (typeof MODELS.options)[number];
+  }>({
     color: COLORS[0],
+    model: MODELS.options[0],
   });
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
@@ -85,35 +96,82 @@ function DesignConfigurator({
             <div className="w-full h-px bg-zinc-200 my-6" />
 
             <div className="relative mt-4 h-full flex flex-col justify-between">
-              <RadioGroup
-                value={options.color}
-                onChange={(val) => {
-                  setOptions((prev) => ({ ...prev, color: val }));
-                }}
-              >
-                <Label>Color: {options.color.label}</Label>
-                <div className="mt-3 flex items-center space-x-3">
-                  {COLORS.map((color) => (
-                    <RadioGroup.Option
-                      key={color.label}
-                      value={color}
-                      className={({ active, checked }) =>
-                        cn(
-                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
-                          { [`border-${color.tw}`]: active || checked }
-                        )
-                      }
-                    >
-                      <span
-                        className={cn(
-                          `bg-${color.tw}`,
-                          "h-8 w-8 rounded-full border border-black border-opacity-10"
-                        )}
-                      />
-                    </RadioGroup.Option>
-                  ))}
+              <div className="flex flex-col gap-6">
+                <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    setOptions((prev) => ({ ...prev, color: val }));
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
+                  <div className="mt-3 flex items-center space-x-3">
+                    {COLORS.map((color) => (
+                      <RadioGroup.Option
+                        key={color.label}
+                        value={color}
+                        className={({ active, checked }) =>
+                          cn(
+                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                            { [`border-${color.tw}`]: active || checked }
+                          )
+                        }
+                      >
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            "h-8 w-8 rounded-full border border-black border-opacity-10"
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+
+                <div className="realtive flex flex-col gap-3 w-full">
+                  <Label>Model</Label>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className="w-full justify-between"
+                      >
+                        {options.model.label}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <ScrollArea className="h-[200px] w-[170px]">
+                        {MODELS.options.map((model) => (
+                          <DropdownMenuItem
+                            key={model.label}
+                            className={cn(
+                              "flex text-sm gap-5 items-center p-1.5 cursor-default hover:bg-zinc-100",
+                              {
+                                "bg-zinc-100":
+                                  model.label === options.model.label,
+                              }
+                            )}
+                            onClick={() =>
+                              setOptions((prev) => ({ ...prev, model }))
+                            }
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                model.label === options.model.label
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                            {model.label}
+                          </DropdownMenuItem>
+                        ))}
+                      </ScrollArea>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-              </RadioGroup>
+              </div>
             </div>
           </div>
         </ScrollArea>
--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -6,5 +6,77 @@ export const COLORS = [
   { label: "Black", value: "black", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
   { label: "Rose", value: "rose", tw: "rose-950" },
-  {label:"Teal",value:"teal",tw:'teal-700'}
+  { label: "Teal", value: "teal", tw: "teal-700" },
 ] as const;
+
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 11 Pro",
+      value: "iphone11pro",
+    },
+    {
+      label: "iPhone 11 Pro max",
+      value: "iphone11promax",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 12 Pro",
+      value: "iphone12pro",
+    },
+    {
+      label: "iPhone 12 Pro max",
+      value: "iphone12promax",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    { label: "iPhone 13 Pro", value: "iphone13pro" },
+    {
+      label: "iPhone 13 Pro max",
+      value: "iphone13promax",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 14 Plus",
+      value: "iphone14plus",
+    },
+    {
+      label: "iPhone 14 Pro",
+      value: "iphone14pro",
+    },
+    {
+      label: "iPhone 14 Pro max",
+      value: "iphone14promax",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+    {
+      label: "iPhone 15 Plus",
+      value: "iphone15plus",
+    },
+    { label: "iPhone 15 Pro", value: "iphone15pro" },
+    {
+      label: "iPhone 15 Pro max",
+      value: "iphone15promax",
+    },
+  ],
+} as const;
