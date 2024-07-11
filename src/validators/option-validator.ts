538c5ed2eaebba1dcc0d8197aa594b36ca6c5ddb
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-07-11 16:37:15 +0330

    feat:added color customization

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,10 @@
+//bg-blue-950 border-blue-950
+//bg-zinc-900 border-zinc-900
+//bg-rose-950 border-rose-950
+//bg-teal-700 border-teal-700
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+  {label:"Teal",value:"teal",tw:'teal-700'}
+] as const;
