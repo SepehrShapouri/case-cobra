2f47564ec29df6929e2ae982e250f8ad25a19993
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-08-18 00:05:39 +0330

    feat:final commit, development finished

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +23,34 @@ export function fromatPrice(price: number) {
     currency: "USD",
   });
   return formatter.format(price);
+}
+
+export function generateMetadata({
+  title = "CaseCobra Custom high-quality phone cases",
+  description = "Create custom high quality phone cases",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+    },
+    icons,
+    metadataBase: new URL("https://case-cobra-sepehrshapouri.vercel.app"),
+  };
 }
